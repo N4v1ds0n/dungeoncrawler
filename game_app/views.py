f6adc8deb0cab63fa092468a4c8c897574db2613
N4v1ds0n <damian.drostemx.de>
2024-11-10 00:40:01 +0100

    more clean up

--- game_app/views.py
+++ game_app/views.py
@@ -1,10 +1,8 @@
-import re
-from datetime import datetime
-
 from flask import Flask
 from flask import render_template
+from datetime import datetime
+from . import app
 
-app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
